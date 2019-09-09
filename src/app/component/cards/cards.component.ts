f283f6e7745f112dcddc767911514a4d5d525dad
vetrichelvim <vetrichelvi@tenacitics.com>
2019-09-09 19:48:52 +0530

    files added

--- src/app/component/cards/cards.component.ts
+++ src/app/component/cards/cards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CardsServiceService } from '../../service/cards-service.service';
 
 @Component({
   selector: 'app-cards',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent implements OnInit {
+  public articles;
 
-  constructor() { }
+  constructor(private _data:CardsServiceService) { }
 
   ngOnInit() {
+    this._data.getNews().subscribe((data)=>{
+      this.articles = data['articles'];
+    })
   }
   openModal(){
     alert('click');
